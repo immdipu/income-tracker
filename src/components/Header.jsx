2ff7f05a91a2513a9d65552728c6d5af9547f3c2
Dipu <dipuchaurasiya91@gmail.com>
2022-11-21 13:39:58 +0545

    title font changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = ({ totalIncome }) => {
   return (
     <div className="w-full  justify-center">
       <div className="header flex justify-between m-auto mt-10 items-center">
-        <h1 className="text-2xl font-semibold text-gray-600">Income Tracker</h1>
+        <h1 className="text-4xl font-semibold text-gray-600">Income Tracker</h1>
         <h1 className="text-3xl font-semibold text-gray-600 bg-gray-300 px-2 py-2 rounded-md">
           ${totalIncome}
         </h1>
